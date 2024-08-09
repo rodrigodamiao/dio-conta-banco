1ebe37b742a6499df59512fb2acb519110fef27f
rodrigodamiao <saskydamiao@gmail.com>
2024-08-09 14:54:08 -0300

    Adicionando o código do desafio.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,15 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class ContaTerminal {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Bem vindo ao sistema do banco, vamos iniciar o processo de identificação.");
+        System.out.println("Digite o seu nome:");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o nome da sua agência:");
+        String agenciaBanco = scanner.next();
+
+        System.out.println("Digite o número da sua conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o saldo da sua conta:");
+        double saldoConta = scanner.nextDouble();
+
+        System.out.println(("Olá, "+nomeCliente+", obrigado por criar uma conta " +
+                "em nosso banco, sua agência é "+agenciaBanco+",conta "+numeroConta+" e seu saldo "+saldoConta+
+                " já está disponível para saque."));
     }
 }
