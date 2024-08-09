460a484969c130973f70b07c7157bba052b2e676
rodrigodamiao <saskydamiao@gmail.com>
2024-08-09 14:57:08 -0300

    Removendo comentários desnecessários.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,8 +1,6 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ContaTerminal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
